d5d589bc217c8f0e858184b5503bf71b049890f5
C. Luce <carol.luce@ocdsb.ca>
2021-09-30 15:22:30 +0000

    tried reorganizing exception clauses

--- main.py
+++ main.py
@@ -6,36 +6,35 @@ class RangeError(Exception):		# The name RangeError can be changed,
 tot=0
 count=0
 while True:
+  try:
     #error handling for illegal int and
     #custom error handling for negative values
-    try:
-        num= int(input ("Enter a positive integer, 0 to quit: "))
-        if num==0:
-            break
-        elif num<0:
-            raise RangeError		# “calling” my custom exception
-        else:
-            tot+=num
-            count+=1
-    except ValueError:			# if the checked exception is raised (built-in Python exception)
-        print ("Not an integer")
-    except RangeError:			# for when my custom exception is raised
-        print ("Not a POSITIVE integer")
-    except Exception:
-        print("Something else went wrong")
-        
+    num= int(input ("Enter a positive integer, 0 to quit: "))
+    if num==0:
+        break
+    elif num<0:
+        raise RangeError		# “calling” my custom exception
+    else:
+        tot+=num
+        count+=1
+
+  except ValueError:			# if the checked exception is raised (built-in Python exception)
+    print ("Not an integer")
+  except RangeError:			# for when my custom exception is raised
+    print ("Not a POSITIVE integer")
+  except Exception:
+    print("Something else went wrong")
+
 #error handling for a division by 0 error (count remains at 0)
 try:
-    print ("Average is",tot/count)
+  print ("Average is",tot/count)
 except ZeroDivisionError:		# another built-in (checked) exception
-    print ("No numbers were entered")
-
-
+    print ("No numbers were entered")        
+except Exception:
+    print("Something else went wrong")
 
 
 # Another separate example as a definition:
-# You may use this to help you write the definition for exercise #11
-
 def getInt(prompt):
     while True:
         try:
